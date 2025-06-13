d60f527aeb65c78aaf04618ae6f2cbd3fdbf5b88
Justus <justus.d2025@gmail.com>
2025-06-13 16:52:46 +0200

    1.14.27 - added default header for video view

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
 from flask import Flask
 
+from bin.letsPlayFile import LetsPlayFile
+from bin.constants import LETSPLAY_PATH
+from os import listdir
 
 from flask import render_template
 from json import load
+from markdown import markdown
 
 app = Flask(__name__)
+
+
+
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/all_videos/')
+def video_show():
+    site = render_template('all_videos.html')
+    
+    OUTPUT_STRING = ""
+    markdown()
+    
+    temp_string = """
+# GAME_NAME
+
+[Image](ICON_PATH)
+
+
+"""
+    
+    
+    
+    lets_plays: list[LetsPlayFile] = [LetsPlayFile(LETSPLAY_PATH + file) for file in listdir(LETSPLAY_PATH) if file.endswith('.json')]
+    
+    for lp in lets_plays:
+        for ep in lp._getEpisodes():
+            pass
+    
+    return site
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ templates/lets_play_header.md
@@ -0,0 +1,10 @@
+# LP_NAME
+
+![IMAGE](ICON_PATH)
+
+## Stats
+
+| Stat | Val |
+| ---- | --- |
+| Length | EPISODE_LENGTH
+| game | GAME_NAME |
