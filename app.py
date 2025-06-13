bb6be5bea1f204c63d30f459e1b1f00a478e79c1
Justus <justus.d2025@gmail.com>
2025-06-13 22:33:53 +0200

    1.14.33

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def video_show():
             
             TMP_EPISODE = TMP_EPISODE.replace('__EP_TITLE__',str(ep['episodeTitle']))
             TMP_EPISODE = TMP_EPISODE.replace('__EP_VIDEO_FILE_SIZE__',str(ep['videoFileSize']) if 'videoFileSize' in ep else 'n.a.')
-            TMP_EPISODE = TMP_EPISODE.replace('__EP_VIDEO_LENGTH__',str(ep['videoLength']))
+            TMP_EPISODE = TMP_EPISODE.replace('__EP_VIDEO_LENGTH__',str(ep['videoLength']) if 'videoLength' in ep else 'n.a.')
             
             TMP_EPISODE = TMP_EPISODE.replace('__EP_MARKER_COUNT__',str(len(ep['markers'])))
             TMP_EPISODE = TMP_EPISODE.replace('__EP_THUMBNAIL_FRAME__',str(ep['thumbnailFrame']))
