2fd210a985e1fa5b5c2be20ec31ed826a75dba34
Justus <justus.d2025@gmail.com>
2025-06-14 22:29:59 +0200

    1.14.48 - added colored text

--- test_obs.py
+++ test_obs.py
@@ -1,9 +1,13 @@
 from bin.obsow import OBSObserver
 from bin.letsPlayFile import LetsPlayFile
+def c_text(text:str, color: tuple) -> str:
+    r,g,b = color
+    return f'\033[38;2;{r};{g};{b}m{text}\033[39m'
 def main():
     
     lpc = LetsPlayFile('C:\\\\Users\\Justus\\jri_data\\lps\\astroneer.json')
     obs = OBSObserver(lpc)
     while 1:
-        print(obs.time)
+        print(obs.time,c_text(obs.time,obs.color))
+        obs.update()
 main()
