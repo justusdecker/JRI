70853ffeb04a91a1da40025f7985db678832f2f0
Justus <justus.d2025@gmail.com>
2025-06-15 00:45:18 +0200

    1.14.57

---
+++ static/img/all_videos.js
@@ -0,0 +1,8 @@
+
+function toggleDisplay() {
+    if (getElementById('tbu___EPISODE_NUMBER__').style.display == 'block') {
+        getElementById('tbu___EPISODE_NUMBER__').style.display = 'none';
+    } else {
+        getElementById('tbu___EPISODE_NUMBER__').style.display = 'block';
+    }
+}
