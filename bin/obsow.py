3c8c159c8b4e678d8ae6436f6e357cbd777dfe6a
Justus <justus.d2025@gmail.com>
2025-06-14 21:51:29 +0200

    1.14.44 - further refactoring

--- bin/obsow.py
+++ bin/obsow.py
@@ -28,15 +28,11 @@ class OBSObserver:
     warnings: int = 0
     obsncw: bool = False
     key_marker_pressed: bool = False
-    login_thread: bool = False
+    login_thread: bool = False # is false if connection establishment is all right otherwise true
     
-    def __init__(self,
-                 currentLP,
-                 current_letsplay_path
-                 ):
-        self.pressedKey = ''
-        self.JTG: ThumbnailGenerator = ThumbnailGenerator()
-        self.current_letsplay_path = current_letsplay_path
+    def __init__(self, lets_play_file: LetsPlayFile):
+        self.pressed_key = ''
+        self.jtg: ThumbnailGenerator = ThumbnailGenerator()
         self.options = {
             'host': SETTINGS._['obsHost'],
             'port': SETTINGS._['obsPort'],
@@ -44,10 +40,9 @@ class OBSObserver:
             'timeout': SETTINGS._['obsTimeout']
             }
         self.connect()
-        self.currentLPData: LetsPlayFile = currentLP
-        self.tna = False
-        self.isPressed = False
-        self.ignoredWarnings = 0
+        self.current_lp_data: LetsPlayFile = lets_play_file
+        self.kb_pressed = False
+        self.ignored_warnings = 0
     
     def connect(self):
         """
@@ -57,27 +52,18 @@ class OBSObserver:
         
             - WindowsError
             - WebSocketTimeoutExeception
-            
-            
         """
         # Try to connect
         try:
             self.obs = obsws.ReqClient(host=self.options['host'], port=self.options['port'], password=self.options['password'],timeout=self.options['timeout'])
-            self.connectionEstablished = True
-            self.obsncw = False
-            self.tna = False
+            self.connection_established = True
         except WindowsError as E: #! Some error occured
             #CrashBox([f'[{ERRORIDS.OBSWSTE}] OBS cant connect {E.errno}', str(E)])
-            self.connectionEstablished = False
-            
-
-            self.obsncw = True
+            self.connection_established = False
         except _exceptions.WebSocketTimeoutException as E: #! Some error occured
             CrashBox([f'[{ERRORIDS.OBSWE}] OBS cant connect [WS-TimeOut]', str(E)])
-            self.connectionEstablished = False
-
-            self.obsncw = True
-        self.loginThread = False
+            self.connection_established = False
+        self.login_thread = False
     @property
     def time(self) -> str:
         """
@@ -86,10 +72,7 @@ class OBSObserver:
         try:
             return str(self.obs.get_output_status(OUTPUT_TYPE)[0]['outputTimecode'])
         except:
-            if not self.tna: #What the fuck is tna? Why did i did this god damnit!?
-                self.tna = True
             return 'OBS is not activated'
-
     @property
     def formatted_time(self) -> int:
         return self.get_rtime_as_int(self.timecode()),self.timecode() if self.is_recording else 0
@@ -100,11 +83,10 @@ class OBSObserver:
     @property
     def current_filepath(self) -> str:
         return str(self.obs.get_output_settings(OUTPUT_TYPE)[0]['outputSettings']['path'])
-    
     @property
     def color(self) -> tuple[int,int,int]:
         """ color based on current state """
-        ep_len = self.currentLPData._getEpisodeLength()
+        ep_len = self.current_lp_data._getEpisodeLength()
         ft = self.formatted_time[0]
         ir = self.is_recording
         
@@ -120,16 +102,15 @@ class OBSObserver:
         tmp_t: list[str] = t.split('.')[0].split(':')
         return (int(tmp_t[0]) * 60 * 60) + (int(tmp_t[1]) * 60) + int(tmp_t[2])
     
-    
-    def onStart(self):
+    def on_start(self):
         """
         processes events "on Start"
         """
         if self.timecode != '00:00:00.000' and not self.is_recording:
             self.is_recording = True
-            self.currentLPData._addEpisode({
+            self.current_lp_data._addEpisode({
                 "path": self.current_filepath,
-                "episodeNumber": self.currentLPData._getEpisodeCount() + 1,
+                "episodeNumber": self.current_lp_data._getEpisodeCount() + 1,
                 "status": 0,
                 "markers": [],
                 "episodeTitle": '',
@@ -139,28 +120,89 @@ class OBSObserver:
                 "audioFilePath": None
             }
             )
-            self.currentLPData.save()
+            self.current_lp_data.save()
+    
+    def on_stop(self):
+        """
+        processes events "on Stop"
+        """
+        if self.is_recording and self.timecode() == '00:00:00.000':
+            self.current_lp_data._setEpisode(-1, 'status', self.current_lp_data._getEpisodeEx(-1,'status') + 1)
+            self.current_lp_data._setEpisode(-1, 'thumbnailPath',f"{THUMBNAIL_PATH}{self.current_lp_data._getName()}\\{self.current_lp_data._getEpisodeCount()}_{self.current_lp_data._getName()}_Thumbnail.png")
+            self.current_lp_data.save()
+            
+            #! VERY UNSAFE Implementation of threads! Make sure that threads are accessible to stop the app from moving on!
+            Thread(target=self.thumbnailGeneration,args=[self.current_lp_data._getEpisodeCount()]).start()
+            Thread(target=self.audioExtraction,args=[self.current_lp_data._getEpisodeCount()]).start()
+            self.is_recording = False
+            self.warnings = False
+
+
+            
+    
+    def setMarker(self):
+        """
+        Append a timeStamp to the current Episode
+        """
+        if is_pressed('K') and not self.kb_pressed and self.is_recording:
+            self.pressed_key = 'K'
+            markers = self.current_lp_data._getEpisodeEx(-1,'markers')
+            markers.append(f'<$TimeLapse:{self.time}>')
+            self.current_lp_data._setEpisode(-1,'markers',markers)
+
+
+            self.current_lp_data.save()
+            self.kb_pressed = True
+        if not is_pressed('K') and self.kb_pressed and self.pressed_key == 'K':
+            self.kb_pressed = False
+        if is_pressed('L') and not self.kb_pressed and self.is_recording:
+            self.pressed_key = 'L'
+            markers = self.current_lp_data._getEpisodeEx(-1,'markers')
+            markers.append(f'<$Interest:{self.time}>')
+            self.current_lp_data._setEpisode(-1,'markers',markers)
+
+            self.current_lp_data.save()
+            self.kb_pressed = True
+        if not is_pressed('L') and self.kb_pressed and self.pressed_key == 'L':
+            self.kb_pressed = False
+        
+    def update(self):
+        try:
+            self.on_start()
+            self.on_stop()
+            self.setMarker()
+        except Exception as E:
+            self.ignored_warnings += 1
+            if self.ignored_warnings > 2:
+                CrashBox([f'[{ERRORIDS.JRICANTCONNECT}] JRI cant connect to OBS', 'Check Settings: IP Adress, Port, Password & timeout.\n And use Advance Settings in OBS!\nJRI close itself!'])
+            else:
+                CrashBox([f'[{ERRORIDS.OBSMO}] OBS moved out', str(E) + str({E.args})])
+            self.connection_established = False
+            if not self.login_thread: 
+                self.login_thread = True
+                Thread(target=self.connect).start()
+    
     
     def thumbnailGeneration(self,epn):
-        DM.createFolder(THUMBNAIL_PATH + self.currentLPData._getName())
-        self.JTG.createThumbnail(self.currentLPData._getEpisodeCount(),
+        DM.createFolder(THUMBNAIL_PATH + self.current_lp_data._getName())
+        self.jtg.createThumbnail(self.current_lp_data._getEpisodeCount(),
                                  self.current_filepath,
                                  -1,
-                                 self.currentLPData._getThumbnailAutomationData(),
-                                 self.currentLPData._getName()
+                                 self.current_lp_data._getThumbnailAutomationData(),
+                                 self.current_lp_data._getName()
                                  )
 
-        self.currentLPData._setEpisode(
+        self.current_lp_data._setEpisode(
             epn - 1,
             'status',
-            self.currentLPData._getEpisodeEx(epn - 1,'status') + 2  # TODO : Use the new ST values
+            self.current_lp_data._getEpisodeEx(epn - 1,'status') + 2  # TODO : Use the new ST values
             )
-        self.currentLPData._setEpisode(
+        self.current_lp_data._setEpisode(
             epn - 1,
             'thumbnailFrame',
-            self.JTG.idx
+            self.jtg.idx
             )
-        self.currentLPData.save()
+        self.current_lp_data.save()
         
     def _cvtAudio(fileName:str,
                  fromType:str= '.mp3',
@@ -179,94 +221,18 @@ class OBSObserver:
     def audioExtraction(self,epn):
         """Audio Extraction out of current Episode Video File"""
         
-        DM.createFolder(f'{AUDIO_PATH}{self.currentLPData._getName()}\\')
-        AFX.extractAudio(self.currentLPData._getEpisodeEx(-1,'path'),f"{AUDIO_PATH}{self.currentLPData._getName()}\\{self.currentLPData._getEpisodeCount()}_{self.currentLPData._getName()}_track.mp3")
+        DM.createFolder(f'{AUDIO_PATH}{self.current_lp_data._getName()}\\')
+        AFX.extractAudio(self.current_lp_data._getEpisodeEx(-1,'path'),f"{AUDIO_PATH}{self.current_lp_data._getName()}\\{self.current_lp_data._getEpisodeCount()}_{self.current_lp_data._getName()}_track.mp3")
 
-        self.currentLPData._setEpisode(
+        self.current_lp_data._setEpisode(
             epn - 1,
             'status',
-            self.currentLPData._getEpisodeEx(epn - 1,'status') + 4 # TODO : Use the new ST values
+            self.current_lp_data._getEpisodeEx(epn - 1,'status') + 4 # TODO : Use the new ST values
             )
-        self.currentLPData._setEpisode(
+        self.current_lp_data._setEpisode(
             epn - 1,
             'audioFilePath',
-            f"{AUDIO_PATH}{self.currentLPData._getName()}\\{epn}_{self.currentLPData._getName()}_track.mp3"
+            f"{AUDIO_PATH}{self.current_lp_data._getName()}\\{epn}_{self.current_lp_data._getName()}_track.mp3"
             )
-        self.currentLPData.save()
+        self.current_lp_data.save()
 
-    def onStop(self):
-        """
-        processes events "on Stop"
-        """
-        if self.is_recording and self.timecode() == '00:00:00.000':
-            self.currentLPData._setEpisode(
-            -1,
-            'status',
-            self.currentLPData._getEpisodeEx(-1,'status') + 1 # TODO : Use the new ST values
-            )
-            self.currentLPData._setEpisode(
-            -1,
-            'thumbnailPath',
-            f"{THUMBNAIL_PATH}{self.currentLPData._getName()}\\{self.currentLPData._getEpisodeCount()}_{self.currentLPData._getName()}_Thumbnail.png"
-            )
-            self.currentLPData.save()
-            
-            Thread(
-                target=self.thumbnailGeneration,
-                args=[
-                    self.currentLPData._getEpisodeCount()
-                    ]
-                ).start()
-            Thread(
-                target=self.audioExtraction,
-                args=[
-                    self.currentLPData._getEpisodeCount()
-                      ]
-                ).start()
-            self.is_recording = False
-            self.warnings = False
-
-
-            
-    
-    def setMarker(self):
-        """
-        Append a timeStamp to the current Episode
-        """
-        if is_pressed('K') and not self.isPressed and self.is_recording:
-            self.pressedKey = 'K'
-            markers = self.currentLPData._getEpisodeEx(-1,'markers')
-            markers.append(f'<$TimeLapse:{self.time}>')
-            self.currentLPData._setEpisode(-1,'markers',markers)
-
-
-            self.currentLPData.save()
-            self.isPressed = True
-        if not is_pressed('K') and self.isPressed and self.pressedKey == 'K':
-            self.isPressed = False
-        if is_pressed('L') and not self.isPressed and self.is_recording:
-            self.pressedKey = 'L'
-            markers = self.currentLPData._getEpisodeEx(-1,'markers')
-            markers.append(f'<$Interest:{self.time}>')
-            self.currentLPData._setEpisode(-1,'markers',markers)
-
-            self.currentLPData.save()
-            self.isPressed = True
-        if not is_pressed('L') and self.isPressed and self.pressedKey == 'L':
-            self.isPressed = False
-        
-    def update(self):
-        try:
-            self.onStart()
-            self.onStop()
-            self.setMarker()
-        except Exception as E:
-            self.ignoredWarnings += 1
-            if self.ignoredWarnings > 2:
-                CrashBox([f'[{ERRORIDS.JRICANTCONNECT}] JRI cant connect to OBS', 'Check Settings: IP Adress, Port, Password & timeout.\n And use Advance Settings in OBS!\nJRI close itself!'])
-            else:
-                CrashBox([f'[{ERRORIDS.OBSMO}] OBS moved out', str(E) + str({E.args})])
-            self.connectionEstablished = False
-            if not self.loginThread: 
-                self.loginThread = True
-                Thread(target=self.connect).start()
