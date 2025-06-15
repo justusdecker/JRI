6ea6a6cbbd58d5c8b43fa22b69c28a5a6172bb08
Justus <justus.d2025@gmail.com>
2025-06-15 23:15:07 +0200

    1.14.67 - fixed #19

--- bin/settings.py
+++ bin/settings.py
@@ -1,36 +1,45 @@
 from bin.dataManagement import DM
 from bin.constants import SettingCollection
-from bin.constants import ABSOLUTE_PATH
+
 class Settings:
     def __init__(self) -> None:
         if not DM.existFile('settings.json'):
             DM.save('settings.json',
                  SettingCollection)
+        self.__settings: dict = DM.loads('settings.json')
+        if not isinstance(self.__settings,dict):
+            raise TypeError('SETTINGS MUST BE A DICT')
         
-        self._ = DM.loads('settings.json')
-        
-        #! Check for each option existing If not create it and save it
-        okay = True
-        for key in SettingCollection.keys():
-            if key not in self._:   #Set Defaul If not exist
-                self._[key] = SettingCollection[key]
-                okay = False
-        if not okay:
-            DM.save('settings.json',self._)
-        if not 'fastLoad' in self._:
-            self._['fastLoad'] = False
-            DM.save('settings.json',self._)
-    def _getWindowSize(self):
-        return self._['width'],self._['height'] 
-    def ce(self,key,dir,default):
-        if not key in dir:
-            return default
-        else:
-            return dir[key]
-    def save(self):
-        DM.save('settings.json',self._)
-    def _unpack(self):
-        self.SIZE = self.ce('width',self._,480),self.ce('height',self._,720)
-        #self.ce('width',self._,480)
+    def save_settings(self) -> None:
+        DM.save('settings.json',{
+            'width': self.width,
+            'height': self.height,
+            'obsDirPath': self.obs_path,
+            'obsHost': self.obs_ip,
+            'obsPassword': self.obs_pw,
+            'obsPort': self.obs_port,
+            'obsTimeout': self.obs_timeout
+        })
+    @property
+    def width(self) -> int:
+        return self.__settings.get('width',1280)
+    @property
+    def height(self) -> int:
+        return self.__settings.get('height',720)
+    @property
+    def obs_path(self) -> str:
+        return self.__settings.get('obsDirPath','C:\\Program Files (x86)\\Steam\\steamapps\\common\\OBS Studio\\bin\\64bit\\')
+    @property
+    def obs_ip(self) -> str:
+        return self.__settings.get('obsHost','')
+    @property
+    def obs_pw(self) -> str:
+        return self.__settings.get('obsPassword','')
+    @property
+    def obs_port(self) -> int:
+        return self.__settings.get('obsPort',4455)
+    @property
+    def obs_timeout(self) -> int:
+        return self.__settings.get('obsTimeout',1)
 
 SETTINGS = Settings()
