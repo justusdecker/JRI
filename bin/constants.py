53808bbf8e8f12c261e8e285f04c6abf8526bc6e
Justus <justus.d2025@gmail.com>
2025-06-16 08:45:21 +0200

    1.14.73 - fixed #33

--- bin/constants.py
+++ bin/constants.py
@@ -173,29 +173,3 @@ class LC:
     TAGS = 'tags'
 
 DAVINCIRESOLVESCRIPTFOLDER = "C:\\Users\\Justus\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Edit"
-
-#! ERRORS
-
-class ERRORIDS:
-    OBSWSTE : int = 1
-    OBSWE : int = 2
-    OBSMO : int = 3
-    LPFNAMEEMPTY : int = 4
-    JRICANTCONNECT : int = 5
-class ERRORDICT:
-    NOLPFEXIST_JRI : list = ['JRI Crashed','No LPF File in destination\ndefault.json created.\nProgram closes!']
-    NOLPFEXIST_JVE : list = ['JVE Crashed','No LPF File in destination']
-    OBSCANTCONNECT : list = ['OBS cant connect', '']
-class Styles:
-    OK =                    0
-    OK_CANCEL =             1
-    ABORT_RETRY_IGNORE =    2
-    YES_NO_CANCEL =         3
-    YES_NO =                4
-    RETRY_NO =              5
-    CANCEL_RETRY_CONTINUE = 6
-class Icons:
-    STOP        =   16
-    QUESTION    =   32
-    EXCLAMATION =   48
-    INFORMATION =   64
