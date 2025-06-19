c4b6e128abcf64991374492d2ee6fbb8ae30501e
Justus <justus.d2025@gmail.com>
2025-06-19 16:27:00 +0200

    1.14.118 - added helper function isnumeric because the built-in str is bs

--- bin/minfuncs.py
+++ bin/minfuncs.py
@@ -11,3 +11,10 @@ def getHexDoubleZeros(val):
     if val.__len__() < 2:
         return '0' + str(val)
     return str(val)
+
+def isnumeric(number: float | int) -> bool:
+    try: 
+        float(number)
+        return True
+    except:
+        return False
