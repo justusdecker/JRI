6d542aa4cef24dc36a38b4562f6356d8a1706785
Justus <justus.d2025@gmail.com>
2025-06-19 16:09:30 +0200

    1.14.117 - added a lot to read into the docstring

--- bin/query/thumbnail_generator_query.py
+++ bin/query/thumbnail_generator_query.py
@@ -11,62 +11,125 @@ class ThumbnailGeneratorQuery:
     .. lp::
         `str` Lets Play Hash
         
-        lp
+        *lp
 
     Text / Font
     -----
     .. px::
         `int` Position X
         
-        pos_x
+        *text_pos_x
     .. py::
         `int` Position Y
         
-        pos_y
+        *text_pos_y
     .. text_font::
         `str` Font Path
         
-        font
+        *font
     .. text_align::
         `str` Alignment **center, left, right**
         
-        text_align
+        *text_align
     .. font_s::
         `float` The Font size
         
-        font_size
+        !font_size
     .. text_r::
         `float` The Font rotation
         
-        text_rotation
+        !text_rotation
     
     Logo
     -----
     .. logo_path:
         `str` Defines the location of the logo image
         
-        logo_path
+        !logo_path
     .. logo_r::
         `float` The Logo rotation
         
-        logo_rot
+        !logo_rot
     .. logo_s::
         `float` The Logo scale
         
-        logo_scale
+        !logo_scale
     .. logo_x::
         `int` The Logo position X
         
-        will be combined with `logo_y` to logo_pos
+        !logo_pos_x
     .. logo_y::
         `int` The Logo position Y
         
-        will be combined with `logo_x` to logo_pos
-    logo_s          float
-    logo_x          int
-    logo_y          int
-    logo_align      str - center, left, right
+        !logo_pos_y
+    .. logo_align::
+        `str` Alignment **center, left, right**
+        
+        !logo_align
     
+    Background
+    -----
+    .. p_x::
+        `int` position X
+        
+        !bg_pos_x
+    .. p_y::
+        `int` position Y
+        
+        !bg_pos_y
+
+    .. rp_x::
+        `int` random position X
+        
+        !bg_pos_x
+    .. rp_y::
+        `int` random position Y
+        
+        !bg_pos_y
+
+    .. r::
+        `float` rotation
+        
+        !bg_rot
+    .. rr_x::
+        `float` random rotation
+        
+        !bg_rrot_x
+    .. rr_y::
+        `float` random rotation
+        
+        !bg_rrot_y
+
+    .. s::
+        `float` scale
+        
+        !bg_rot
+    .. rs_x::
+        `float` random scale
+        
+        !bg_rscale_x
+    .. rs_y::
+        `float` random scale
+        
+        !bg_rscale_y
+    
+    .. hue::
+        `float` the hue value of each pixel manipulation in the image
+        
+        !hue
+    .. sat::
+        `float` the saturation value of each pixel manipulation in the image
+        
+        !sat
+    .. lig::
+        `float` the lightness value of each pixel manipulation in the image
+        
+        !lig
+
+    .. background::
+     `str` Defines the location of the background image(If not set the image will be fetched out of the video!)
+     
+        !bg
     """
     def __init__(self,query: str):
         self.query = {}
@@ -80,6 +143,9 @@ class ThumbnailGeneratorQuery:
     @property
     def font(self) -> str:
         return self.query.get('text_font', '')
+    @property
+    def text_align(self) -> str:
+        return self.query.get('text_align', 'center')
     @property
     def __text_px(self) -> str:
         return self.query.get('px', '')
