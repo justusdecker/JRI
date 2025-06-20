45711b36ca1fc2f94155e2b5e916252c3498c51a
Justus <justus.d2025@gmail.com>
2025-06-21 01:32:12 +0200

    1.14.151 - added LPE

--- bin/lp_data_management.py
+++ bin/lp_data_management.py
@@ -1,10 +1,14 @@
 from bin.jf_filereader import JFFile
-
+# Uploadat will be replaced with:
+# Header::start_date
+# Header::time_delta_between_uploads
+#
+# This will reduce filesize & make it more readable
 class LetsPlay:
     def __init__(self, filepath: str):
         self.jf = JFFile(filepath)
         self.header = LetsPlayHeader(self.jf)
-
+        self.episodes = [LetsPlayEpisode(i) for i in self.jf.get('<EP>::EPISODES')]
 class LetsPlayHeader:
     def __init__(self, jf: JFFile):
         self.jf = jf
@@ -36,9 +40,29 @@ class LetsPlayHeader:
     @episode_length.setter
     def episode_length(self, value: int):
         self.jf.set('<HEADER>::EPISODE_LENGTH', value)
-    
+
 class LetsPlayEpisode:
     def __init__(self):
+        """
+        A Lets Play Episode must be formatted like:
+        - video_path
+        - audio_path
+        - thumbnail_path
+        - title
+        - episode_number
+        
+        Generate:
+        - video_length
+        - audio_length
+        - video_file_size
+        - audio_file_size
+        
+        Status will be replaced by:
+        - video_exist / not ""
+        - audio_exist / not ""
+        - thumbnail_exist / not ""
+        
+        """
         pass
 
 class LetsPlayTADText:
