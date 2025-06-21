c417c74a038a77160c4fff5a5803aa6ec4825e25
Justus <justus.d2025@gmail.com>
2025-06-21 12:00:49 +0200

    1.14.154 - start switching to sql

---
+++ bin/sql_hookup.py
@@ -0,0 +1,3 @@
+import sqlite3
+
+CONNECTION = sqlite3.connect('test.db')
--- debug_fix_image_problem.py
+++
@@ -1,9 +0,0 @@
-from bin.letsplay_file import LetsPlayFile
-
-from bin.constants import PATHS
-from os import listdir
-LPF = [LetsPlayFile(PATHS.letsplay + file) for file in listdir(PATHS.letsplay) if file.endswith('.json')]
-
-for lp in LPF:
-    print(lp.asdict())
-    
